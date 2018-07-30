997cb9a7b051d5e75971cb817e73655222e4fae9
Elijah Nicasio <ElijahN8@gmail.com>
2018-07-30 14:04:11 -0700

    fixed typo

--- lab1/Lab1_Part2/Lab1_Part2/main.c
+++ lab1/Lab1_Part2/Lab1_Part2/main.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs,
+	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs,
 	// initialize to 0s
 	
 	while(1)
